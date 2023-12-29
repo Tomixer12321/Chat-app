7a7c85ead1ecdbc80823efeb7c1f81eb876eeb85
Tomixer <tomix.klokar@gmail.com>
2023-12-29 20:06:23 +0100

    create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>LOGIN</h1>
+      <input type="email" placeHolder="email" /> <br />
+      <input type="password" placeHolder="password" /> <br />
+      <input type="submit" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
