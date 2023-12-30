8fb9f93b54c14dc703cb000c64d1409a84a4cee0
Tomixer <tomix.klokar@gmail.com>
2023-12-30 21:13:15 +0100

    upgrade

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,72 @@
-import { Link } from "react-router-dom"
+import "./Login.css";
+import { Link } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 const Login = () => {
   return (
-    <div>
-      <h1>LOGIN</h1>
-      <input type="email" placeHolder="email" /> <br />
-      <input type="password" placeHolder="password" /> <br />
-      <input type="submit" />
+    <Box className="login-container">
+      <Typography
+        component="h1"
+        variant="h4"
+        sx={{
+          fontFamily: "Oxanium, cursive",
+          fontWeight: 600,
+          marginBottom: 4,
+        }}
+      >
+        LOGIN
+      </Typography>
+      <TextField
+        id="Email"
+        label="Email"
+        variant="outlined"
+        sx={{
+          width: "18%",
+          "& .MuiOutlinedInput-root": {
+            "&.Mui-focused fieldset": {
+              borderColor: "#ed288b",
+            },
+          },
+          "& .MuiInputLabel-outlined.Mui-focused": {
+            color: "#ed288b",
+          },
+        }}
+      />
+      <br />
+      <TextField
+        id="Password"
+        label="Password"
+        variant="outlined"
+        sx={{
+          width: "18%",
+          "& .MuiOutlinedInput-root": {
+            "&.Mui-focused fieldset": {
+              borderColor: "#ed288b",
+            },
+          },
+          "& .MuiInputLabel-outlined.Mui-focused": {
+            color: "#ed288b",
+          },
+        }}
+      />
+      <br />
+      <FormControlLabel
+        control={<Checkbox defaultChecked sx={{color:"#ed288b"}} />}
+        label="Remember me"
+      />
+      <br />
       <Link to="/register">
-        <input type="submit" value="register" />
+        <Button variant="contained" sx={{fontSize:"1rem",width:"22rem",background:"#ed288b"}}>
+          Login
+          </Button>
       </Link>
-    </div>
-  )
-}
+    </Box>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import TextField from '@mui/material/TextField';
 
 const register = () => {
   return (
