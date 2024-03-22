f284a6fab0c43f92f60100953fee41296f180a50
Tomixer <tomix.klokar@gmail.com>
2024-03-22 14:28:40 +0100

    frontend update

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -4,9 +4,14 @@ import IconButton from "@mui/material/IconButton";
 import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
 import httpRequest from "../../httpRequest";
 import userContext from "../../context/user-context";
 import "./Home.css";
+
+const settings = ["Logout"];
 
 const Home = () => {
   const userCtx = useContext(userContext);
@@ -15,6 +20,10 @@ const Home = () => {
 
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   useEffect(() => {
@@ -30,16 +39,40 @@ const Home = () => {
       }
     })();
   }, [userCtx]);
-
+    
   return (
     <div className="gray-box">
-      <Box sx={{ flexGrow: 0 }}>
-        <Tooltip title="Open settings">
-          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-          </IconButton>
-        </Tooltip>
-      </Box>
+      <div className="box-wrapper">
+        <Box sx={{ position: "relative" }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Tomixer" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </div>
       <div className="content-wrapper">
         <TextField
           id="filled-basic"
