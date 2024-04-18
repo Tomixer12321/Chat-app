600c831683712de64a8a648379166accaf208ca3
Tomixer <tomix.klokar@gmail.com>
2024-04-18 19:44:28 +0200

    create neew component ChatRoom.js

---
+++ frontend/src/components/ChatRoom.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ChatRoom = ({ chatroomId }) => {
+  return (
+    <div>
+      <h2>Chat Room {chatroomId}</h2>
+      {/* Sem môžeme pridať logiku na zobrazenie chatovacej miestnosti */}
+    </div>
+  );
+};
+
+export default ChatRoom;
